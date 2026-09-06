c4b4beb88a22f8635f73eaebf7e2c60e4829b98d
HectorAnthonieMuñoz <143550504+HectorAnthonieMunoz@users.noreply.github.com>
2026-09-06 16:18:49 -0300

    Update main.py

--- Servidor/main.py
+++ Servidor/main.py
@@ -1,6 +1,7 @@
 import threading
 import tcp_server
 import udp_server
+# Punto de entrada único que levanta ambos servidores (TCP y UDP) en el mismo proceso
 if __name__ == "__main__":
     threading.Thread(target=udp_server.main, daemon=True).start()
     tcp_server.main()
