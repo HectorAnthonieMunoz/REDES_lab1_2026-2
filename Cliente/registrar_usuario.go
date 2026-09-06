553a9614ff6da451ec9dab9bcb598e8ca0485587
ToonLucas22 <lucas.ignacio.morrison.osorio@gmail.com>
2026-09-06 02:40:29 -0300

    Add user registration functionality in registrar_usuario.go

---
+++ Cliente/registrar_usuario.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	//"net"
+	"net/http"
+	//"sync"
+	//"time"
+	//"os"
+	"fmt"
+	//"bufio"
+	"strings"
+	//"encoding/csv"
+)
+
+var clienteHTTP = http.DefaultClient;
+
+func main() {
+	url := "http://localhost:8888/register";
+
+	var username,password string;
+
+	fmt.Scan(&username, &password);
+
+	body := fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password)
+
+	//fmt.Println(body)
+
+	r, err := http.NewRequest("POST", url, strings.NewReader(body))
+	if (err != nil) {
+		panic(err)
+	}
+	r.Header.Add("Content-Type", "application/json")
+
+	resp, err := clienteHTTP.Do(r);
+	if (err != nil) {
+		panic(err)
+	}
+
+	code := resp.Status;
+
+	fmt.Println(code)
+
+}
