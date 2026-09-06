7337302671bd8722923df7e77e2821d7c0884239
ToonLucas22 <lucas.ignacio.morrison.osorio@gmail.com>
2026-09-06 02:56:05 -0300

    Implementación correcta del TCP

--- Cliente/cliente_tcp.go
+++ Cliente/cliente_tcp.go
@@ -1,43 +1,56 @@
 package main
 
 import (
-	//"net"
-	"net/http"
+	"net"
+	//"net/http"
 	//"sync"
 	//"time"
-	//"os"
+	"os"
 	"fmt"
 	//"bufio"
-	"strings"
+	//"strings"
 	//"encoding/csv"
 )
 
-var clienteHTTP = http.DefaultClient;
 
 func main() {
-	url := "http://0.0.0.0:9000";
+	fmt.Println("Ingrese sus credenciales:")
 
-	var username,nombre string;
+	var username,password string;
 
-	fmt.Scan(&username, &nombre);
+	fmt.Scan(&username, &password)
 
-	body := fmt.Sprintf(`%s %s`, username, nombre)
-
-	//fmt.Println(body)
-
-	r, err := http.NewRequest("LOGIN", url, strings.NewReader(body))
-	if (err != nil) {
-		panic(err)
-	}
-	r.Header.Add("Content-Type", "text")
-
-	resp, err := clienteHTTP.Do(r);
-	if (err != nil) {
-		panic(err)
+	strEcho := fmt.Sprintf("LOGIN %s %s\n", username, password);
+	servAddr := "0.0.0.0:9000"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
 	}
 
-	code := resp.Status;
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
 
-	fmt.Println(code)
+	_, err = conn.Write([]byte(strEcho))
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
 
+	println("write to server = ", strEcho)
+
+	reply := make([]byte, 4096)
+
+	_, err = conn.Read(reply)
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("reply from server=", string(reply))
+
+	conn.Close()
 }
